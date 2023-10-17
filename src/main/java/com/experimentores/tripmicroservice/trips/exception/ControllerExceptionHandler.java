a1b50e8dcc381489080f0d8927ce84936485d7b6
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2023-10-17 17:00:42 -0500

    update delete responses

--- src/main/java/com/experimentores/tripmicroservice/trips/exception/ControllerExceptionHandler.java
+++ src/main/java/com/experimentores/tripmicroservice/trips/exception/ControllerExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 
@@ -15,28 +14,26 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        ErrorMessage message = new ErrorMessage(
+        return new ErrorMessage(
                 HttpStatus.NOT_FOUND.value(),
                 ex.getMessage(),
                 request.getDescription(false),
                 LocalDateTime.now());
-        return message;
     }
 
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ErrorMessage validationException(ValidationException ex, WebRequest request) {
-        ErrorMessage message = new ErrorMessage(
+        return new ErrorMessage(
                 HttpStatus.BAD_REQUEST.value(),
                 ex.getMessage(),
                 request.getDescription(false),
                 LocalDateTime.now());
-        return message;
     }
 
     @ExceptionHandler(InvalidCreateResourceException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ErrorMessage createBodyException(InvalidCreateResourceException e, WebRequest request) {
+    public ErrorMessage invalidCreateResourceException(InvalidCreateResourceException e, WebRequest request) {
         return new ErrorMessage(
                 HttpStatus.BAD_REQUEST.value(),
                 e.getMessage(),
@@ -47,12 +44,11 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorMessage globalExceptionHandler(Exception ex, WebRequest request) {
-        ErrorMessage message = new ErrorMessage(
+        return new ErrorMessage(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 ex.getMessage(),
                 request.getDescription(false),
                 LocalDateTime.now());
-        return message;
     }
 
 
